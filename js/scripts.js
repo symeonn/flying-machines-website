3ace8f545709859b4f86b4a631ea912178acee6c
Mario <symeonn@gmail.com>
2021-07-26 21:17:11 +0200

    purge w3 styles

--- js/scripts.js
+++ js/scripts.js
@@ -70,7 +70,7 @@ function unfade(element) {
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
+    var navbar = document.getElementById("mainNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
